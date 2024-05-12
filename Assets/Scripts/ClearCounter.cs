69ab8ba5b2ffe8448d9c7eff100a708e27b0f8bd
Daniel Malcher <45374834+Desbearu@users.noreply.github.com>
2024-05-12 01:17:41 -0400

    Player Pick Up, Drop Objects

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -8,6 +8,16 @@ public class ClearCounter : BaseCounter
     [SerializeField]private ClearCounter secondClearCounter;
 
     public override void Interact(Player player){
-        
+        if(!HasKitchenObject()){
+            //No Kitchen Object on the Counter
+            if(player.HasKitchenObject()){
+                //PLayer is carrying something
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+        } else {
+         if(!player.HasKitchenObject()){
+            GetKitchenObject().SetKitchenObjectParent(player);
+         }
+        }
     }
 }
