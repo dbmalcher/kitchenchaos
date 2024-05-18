14df31b12e9f87a2e8efd0c162dd8e3634cc7b29
Daniel Malcher <45374834+Desbearu@users.noreply.github.com>
2024-05-18 11:55:43 -0400

    Plate Complete Visual

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlateKitchenObject : KitchenObject
 {
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs : EventArgs{
+        public KitchenObjectSO kitchenObjectSO;
+    }
 
     [SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList;
 
@@ -22,6 +27,11 @@ public class PlateKitchenObject : KitchenObject
             return false;
         } else {
             kitchenObjectSOList.Add(kitchenObjectSO);
+
+            OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs{
+                kitchenObjectSO = kitchenObjectSO
+            });
+
             return true;
         }
     }
