2425dfcd4629ec644470b39350b986679a99ebae
Daniel Malcher <45374834+Desbearu@users.noreply.github.com>
2024-05-25 12:10:06 -0400

    Main Menu, Loading Scene

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class Loader{
+    private static Scene targetScene;
+    public enum Scene{
+        MainMenu,
+        SampleScene,
+        LoadingScene
+    }
+
+    public static void Load(Scene targetScene){
+        Loader.targetScene = targetScene;
+
+        SceneManager.LoadScene(Scene.LoadingScene.ToString());
+    }
+
+    public static void LoaderCallback(){
+        SceneManager.LoadScene(targetScene.ToString());
+    }
+}
---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button quitButton;
+
+    void Awake()
+    {
+        playButton.onClick.AddListener( () => {
+            Loader.Load(Loader.Scene.SampleScene);
+        });
+        
+        quitButton.onClick.AddListener( () => {
+            Application.Quit();
+        });
+    }
+}
